fdd892966e2719210e9002f22a090e715c23dbc0
James McConnaughey <jbmcconnaughey@gmail.com>
2023-08-08 09:12:29 -0400

    final pieces added to webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//Added logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Data PUT to database');
   const jateDb = await openDB('jate', 1);
@@ -23,7 +23,7 @@ export const putDb = async (content) => {
   console.log('Data saved to database', result.value);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Added logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
